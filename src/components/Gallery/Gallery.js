32330face4545ae3ff01ed75b6611c37ecce152c
b4kii <dami4nhalibart@gmail.com>
2022-07-04 12:43:05 +0200

    Change arrows, appear animation on scroll

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,55 +1,36 @@
-// import React, { useState } from "react";
-import React from "react";
-import { useState } from "react";
+import React, { useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
+import Images from "./Images";
 import styles from "../../styles/Gallery.module.css";
-import { images } from "./Images";
+
+const boxVariant = {
+  visible: { y: 0, transition: {duration: 1, type: "spring", bounce: 0.3}},
+  hidden: { y: "80%"},
+};
 
 export default function Gallery() {
-  const [currentPhoto, setCurrentPhoto] = useState(0);
-  const photos = document.getElementsByClassName(styles["photo"]);
+  const control = useAnimation();
+  const [ref, inView] = useInView();
 
-  function changePhoto(next) {
-    if (next >= photos.length) next = 0;
-    if (next < 0) next = photos.length - 1;
-
-    photos[currentPhoto].classList.toggle(styles["active"]);
-    photos[next].classList.toggle(styles["active"]);
-    setCurrentPhoto(next);
-  }
+  useEffect(() => {
+    if (inView) {
+      control.start("visible");
+    }
+  }, [control, inView]);
 
   return (
     <section id="gallery" className={styles["gallery-section"]}>
-      <div className={styles["images-wrapper"]}>
-        <span
-          className={styles["left-arrow"]}
-          onClick={() => {
-            setCurrentPhoto(currentPhoto - 1);
-            changePhoto(currentPhoto - 1);
-          }}
-        >
-          <img src="https://img.icons8.com/ultraviolet/64/undefined/chevron-left.png" />
-        </span>
-
-        <span
-          className={styles["right-arrow"]}
-          onClick={() => {
-            setCurrentPhoto(currentPhoto + 1);
-            changePhoto(currentPhoto + 1);
-          }}
-        >
-          <img src="https://img.icons8.com/ultraviolet/64/undefined/chevron-right.png" />
-        </span>
-
-        {images.map((image) => (
-          <img
-            id={image.id}
-            className={`${styles.photo} ${styles[image.state]}`}
-            src={image.source}
-            key={image.id}
-          />
-        ))}
-      </div>
+      <motion.div
+        className={styles["images-wrapper"]}
+        ref={ref}
+        variants={boxVariant}
+        initial="hidden"
+        animate={control}
+      >
+        <Images />
+      </motion.div>
     </section>
   );
 }
--- src/components/Gallery/Images.js
+++ src/components/Gallery/Images.js
@@ -1,4 +1,7 @@
+import React, {useState} from "react";
+
 import { Bird, Flower, Rice, Strawberries, Cat, Dog } from "../../assets/";
+import styles from "../../styles/Gallery.module.css";
 
 export const images = [
   { source: Bird, id: "image1", state: "active" },
@@ -8,3 +11,50 @@ export const images = [
   { source: Dog, id: "image5", state: "" },
   { source: Cat, id: "image6", state: "" }
 ];
+
+export default function Images() {
+  const [currentPhoto, setCurrentPhoto] = useState(0);
+  const photos = document.getElementsByClassName(styles["photo"]);
+
+  function changePhoto(next) {
+    if (next >= photos.length) next = 0;
+    if (next < 0) next = photos.length - 1;
+
+    photos[currentPhoto].classList.toggle(styles["active"]);
+    photos[next].classList.toggle(styles["active"]);
+    setCurrentPhoto(next);
+  }
+
+  return (
+    <>
+        <span
+          className={styles["left-arrow"]}
+          onClick={() => {
+            setCurrentPhoto(currentPhoto - 1);
+            changePhoto(currentPhoto - 1);
+          }}
+        >
+          <img src="https://img.icons8.com/external-tanah-basah-glyph-tanah-basah/64/000000/external-Chevron-arrows-tanah-basah-glyph-tanah-basah-9.png" />
+        </span>
+
+        <span
+          className={styles["right-arrow"]}
+          onClick={() => {
+            setCurrentPhoto(currentPhoto + 1);
+            changePhoto(currentPhoto + 1);
+          }}
+        >
+          <img src="https://img.icons8.com/external-tanah-basah-glyph-tanah-basah/64/000000/external-Right-arrows-tanah-basah-glyph-tanah-basah-3.png" />
+        </span>
+
+        {images.map((image) => (
+          <img
+            id={image.id}
+            className={`${styles.photo} ${styles[image.state]}`}
+            src={image.source}
+            key={image.id}
+          />
+        ))}
+    </>
+  );
+}
