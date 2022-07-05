061b33504f96d158dd6a60a84c9c3f0df17b28d7
b4kii <dami4nhalibart@gmail.com>
2022-07-06 00:23:26 +0200

    add error handling while fetching

--- src/components/Faq/Quotes.js
+++ src/components/Faq/Quotes.js
@@ -8,39 +8,47 @@ const URL = "https://type.fit/api/quotes";
 export default function Quotes() {
   const [quote, setQuote] = useState([]);
   const [animate, setAnimate] = useState(true);
+  const [error, setError] = useState(false);
 
   let data = [];
 
+  // TODO: Error handling
   const getQuotes = async () => {
-    const response = await fetch(URL);
-    data = await response.json();
-    setQuote((quote) => data[Math.floor(Math.random() * (data.length - 1))]); //
+    try {
+      const response = await fetch(URL);
+      if (!response.ok) {
+        throw new Error(`Test ${response.status}`);
+      };
+      data = await response.json();
+      setQuote((quote) => data[Math.floor(Math.random() * (data.length - 1))]);
+      setError(null);
+    } catch (err) {
+      setError(err.message);
+      setQuote(null);
+    }
   };
-
-  function display() {
-    document.getElementById("quotes-wrapper")
-  }
 
   useEffect(() => {
     getQuotes();
     const interval = setInterval(() => {
       setQuote((quote) => data[Math.floor(Math.random() * (data.length - 1))]);
-      setAnimate((animate) => animate = true);
+      setAnimate((animate) => (animate = true));
     }, 1000 * 5);
 
     return () => {
       clearInterval(interval);
     };
   }, []);
+
   return (
     <div id="quotes" className={styles["quotes-section"]}>
       <div
         id="quotes-wrapper"
-        onAnimationEnd={() => setAnimate((animate) => animate = false)}
+        onAnimationEnd={() => setAnimate((animate) => (animate = false))}
         className={`${styles.wrapper} ${animate ? styles.start : ""}`}
       >
-        <p className={styles.text}>{quote.text}</p>
-        <small className={styles.author}>{quote.author}</small>
+        <p className={styles.text}>{error ? error : quote.text }</p>
+        <small className={styles.author}>{error ? error : quote.author}</small>
       </div>
     </div>
   );
